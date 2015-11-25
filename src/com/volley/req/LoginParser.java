8f1e5b1d5f12ab8f5ea6d7a69f5aa16134c0f34a
czqwsm <1148392049@qq.com>
2015-11-25 10:46:58 +0800

    网络请求，优化。

---
+++ src/com/volley/req/LoginParser.java
@@ -0,0 +1,22 @@
+package com.volley.req;
+
+import com.google.gson.reflect.TypeToken;
+import com.volley.req.net.inferface.IParser;
+import com.volley.req.parser.JsonParserBase;
+import com.volley.req.parser.ParserUtil;
+import org.json.JSONObject;
+
+public class LoginParser implements IParser {
+	@Override
+	public Object fromJson(JSONObject object) {
+		return null;
+	}
+
+	@Override
+	public JsonParserBase<UserInfo> fromJson(String json) {
+		JsonParserBase<UserInfo> result = ParserUtil.fromJsonBase(json, new TypeToken<JsonParserBase<UserInfo>>() {
+		}.getType());
+		return result;
+	}
+}
+
